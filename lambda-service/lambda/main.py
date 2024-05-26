0e5145c38041f440873e29ec7df842a38952303c
QUANG HUY CHU <cqhofsns@gmail.com>
2024-05-27 00:45:49 +0900

    Basic chat 20240527 0044

--- lambda-service/lambda/main.py
+++ lambda-service/lambda/main.py
@@ -1,32 +1,44 @@
 #from translate_model import translate_engine
 from googletrans import Translator
 
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+
+import os
 import json
 import boto3
 
-def translate_message(text):
+LINE_CHANNEL_ACCESS_TOKEN= os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+LINE_BOT_API= LineBotApi(channel_access_token= LINE_CHANNEL_ACCESS_TOKEN)
+
+def translated_function(text):
     translator= Translator()
     return translator.translate(text, dest= "ja", src= "vi")
 
 def handler(event, context):
-    body= json.loads(event["body"])
-    message= body["events"][0]["message"]["text"]
+    try:
+        print(event)
+        body= json.loads(event["body"])
+        if body["events"]:
+            if body["events"][0]["type"]== "message" and body["events"][0]["message"]["type"]== "text":
 
-    translated_message= translated_message(text= message)
+                replyToken= body["events"][0]["replyToken"]
+                message= body["events"][0]["message"]["text"]
 
-    response= {
+                translated_message= translated_function(text= message).text
+
+                LINE_BOT_API.reply_message(
+                    reply_token= replyToken,
+                    messages= TextSendMessage(text= translated_message)
+                )
+    except Exception as error:
+        print(error)
+        return {
+            "statusCode": 500,
+            "body": json.dumps(f"An error occured: {error}")
+        }
+    
+    return {
         "statusCode": 200,
-        "body": json.dumps(
-            {
-                "replyToken": body["events"][0]["replyToken"],
-                "messages": [
-                    {
-                        "type": "text",
-                        "text": translated_message
-                    }
-                ]
-            }
-        )
+        "body": json.dumps("Reply sent")
     }
-
-    return response
